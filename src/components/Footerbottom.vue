cc703b6012685a1d1279ec0342940e802a80d4e3
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-24 17:48:46 +0100

    Footer bottom completed

--- src/components/Footerbottom.vue
+++ src/components/Footerbottom.vue
@@ -1,45 +1,46 @@
 <template>
     <section id="footerbottom">
-        <div class="container">
-            <div>
-                <button>
+        <div class="container" id="contenitore_footer_bottom">
+            <div>   
+                 <button>
                     Sign-UP NOW!
                 </button>
-
-                <nav>
-                    <div>
-                        FOLLOW US
-                    </div>
-                    <ul>
-                        <li>
-                            <a href="#">
-                                f
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                t
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                y
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                s
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                pp
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
             </div>
-            
+           
+
+            <nav>
+                <div>
+                    FOLLOW US
+                </div>
+                <ul>
+                    
+                    <li>
+                        <a href="#">
+                            <img src="../../public/assets/img/footer-facebook.png" alt="facebook logo">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="../../public/assets/img/footer-twitter.png" alt="facebook logo">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="../../public/assets/img/footer-youtube.png" alt="facebook logo">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="../../public/assets/img/footer-pinterest.png" alt="facebook logo">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="../../public/assets/img/footer-periscope.png" alt="facebook logo">
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </section>
     
@@ -56,6 +57,35 @@ export default {
 <style scoped>
     #footerbottom{
         background-color: rgb(27, 27, 27);
+    }
+    #contenitore_footer_bottom{
+        display: flex;
         height: 100px;
+        align-items: center;
+        justify-content: space-between;
+    }
+    button{
+        padding: 4px;
+        border: 2px solid rgb(0, 102, 255);
+        background-color: rgba(0, 0, 0, 0);
+        color: white;
+        font-weight: bold;
+    }
+    nav{
+        display: flex;
+        align-items: center;
+        color: rgb(0, 102, 255);
+        font-weight: bold;
+    }
+    nav ul {
+        display: flex;
+        
+    }
+    ul li{
+        margin: 0 20px;
+        list-style: none;
+    }
+    li a {
+        text-decoration: none;
     }
 </style>
